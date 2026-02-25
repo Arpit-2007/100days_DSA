a0a5ce3574a3112aef8bb91f10c4ae7b5393bd38
Arpit-2007 <arpitsingh1132007@gmail.com>
2026-02-25 09:25:51 +0530

    Create Day25.c

---
+++ Day25.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int value;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* current = head;
+
+    while (current != NULL) {
+        if (current->data == key) {
+            count++;
+        }
+        current = current->next;
+    }
+    return count;
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    int result = countOccurrences(head, key);
+    printf("%d", result);
+
+    return 0;
+}
