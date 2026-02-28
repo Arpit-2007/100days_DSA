9b0c689174aaaf73384a2b0ff281ea42f7ff823c
Arpit-2007 <arpitsingh1132007@gmail.com>
2026-02-28 21:34:09 +0530

    Create Day28.c

---
+++ Day28.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, x;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Input number of nodes
+    scanf("%d", &n);
+
+    if (n <= 0) return 0;
+
+    // Create circular linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    // Traverse circular linked list
+    struct Node* ptr = head;
+    if (head != NULL) {
+        do {
+            printf("%d ", ptr->data);
+            ptr = ptr->next;
+        } while (ptr != head);
+    }
+
+    return 0;
+}
